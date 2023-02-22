d8571136258d6718498c6c70f700589adfb77b3b
Evgenii <evgeniif87@yandex.ru>
2023-02-22 22:33:57 +0200

    modified:   src/views/HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,58 +1,18 @@
 <template>
-  <router-link :to="
-    {
-      path: '/', 
-      query: {
-        status: this.getStatusValue, 
-        sorted: this.getSortedValue,
-        comfirmed_orders_from: this.getComfirmedOrdersFrom,
-        comfirmed_orders_to: this.getComfirmedOrdersTo,
-        order: this.getOrderValue
-      }
-    }
-  ">
-    RESULT
-  </router-link>
+	<FilterBar />
 
-  <FilterBar />
-
-  <Table />
+	<Table />
 </template>
 
 <script>
-import Table from '../components/Table/Table.vue';
-import FilterBar from '../components/FilterBar/FilterBar.vue';
+import Table from "../components/Table/Table.vue"
+import FilterBar from "../components/FilterBar/FilterBar.vue"
 export default {
-  components: {
-    Table, FilterBar
-  },
-
-  computed: {
-    getStatusValue() {
-      return this.$store.getters.STATUS_VALUE
-    },
-
-    getSortedValue() {
-      return this.$store.getters.SORTED_VALUE
-    },
-
-    getOrderValue() {
-      return this.$store.getters.ORDER_VALUE
-    },
-
-    getComfirmedOrdersFrom() {
-      return this.$store.getters.COMFIRMED_ORDERS_FROM
-    },
-
-    getComfirmedOrdersTo() {
-      return this.$store.getters.COMFIRMED_ORDERS_TO
-    }
-  }
+	components: {
+		Table,
+		FilterBar,
+	},
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
-
-
+<style lang="scss" scoped></style>
